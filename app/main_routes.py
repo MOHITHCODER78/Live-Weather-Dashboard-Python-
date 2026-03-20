1f7c4363ecef54d520de1e3d270a98684f2b0198
Mohith Naidu <mohithnaidu7806@gmai.com>
2026-03-20 15:06:36 +0530

    Enhance SEO with robots.txt, sitemap, and Open Graph meta tags

--- app/main_routes.py
+++ app/main_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory, request
 from flask_login import current_user
 import os
 
@@ -12,3 +12,8 @@ def landing():
 def index():
     api_key = os.getenv('OPENWEATHER_API_KEY', '')
     return render_template('index.html', api_key=api_key)
+
+@main_bp.route('/robots.txt')
+@main_bp.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(os.path.join(main_bp.root_path, '..'), request.path[1:])
